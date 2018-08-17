bd994d5d21f511984273111ba4063848051d230c
Damien Abeloos <dabeloos@w-r6-p3.s19.be>
2018-08-17 11:49:26 +0200

    ex06 ft_destroy

---
+++ ex06/ft_destroy.c
@@ -0,0 +1,42 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_destroy.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dabeloos <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/08/17 11:14:38 by dabeloos          #+#    #+#             */
+/*   Updated: 2018/08/17 11:48:49 by dabeloos         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ft_ultimator.h"
+#include <stlib.h>
+#include <stdio.h>
+
+void	ft_destroy(char ***factory)
+{
+	int		i;
+	int		j;
+	int		k;
+
+	i = 0;
+	while (factory[i] != NULL)
+	{
+		j = 0;
+		while (factory[i][j] != NULL)
+		{
+			k = 0;
+			while (factory[i][j][k] != NULL)
+			{
+				free(factory[i][j][k]);
+				k++;
+			}
+			free(factory[i][j]);
+			j++;
+		}
+		free(factory[i]);
+		i++;
+	}
+	free(factory);
+}
