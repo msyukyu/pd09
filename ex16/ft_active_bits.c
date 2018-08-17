ecb04b174a02459daa546c83c9db9c21682ffe92
Damien Abeloos <dabeloos@w-r6-p3.s19.be>
2018-08-17 16:57:09 +0200

    ex16

--- ex16/ft_active_bits.c
+++ ex16/ft_active_bits.c
@@ -6,23 +6,21 @@
 /*   By: dabeloos <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/08/17 16:35:03 by dabeloos          #+#    #+#             */
-/*   Updated: 2018/08/17 16:47:43 by dabeloos         ###   ########.fr       */
+/*   Updated: 2018/08/17 16:56:55 by dabeloos         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 unsigned int	ft_active_bits(int value)
 {
 	unsigned int	result;
-	unsigned int	i;
-	unsigned int	power;
+	unsigned int	count;
 
+	count = 0;
 	result = (unsigned int)value;
-	i = 1;
-	power = 1;
-	while (power < value)
+	while (!(value / 2 == 0 && value % 2 == 0))
 	{
-		power *= 2;
-		i++;
+		count += value % 2;
+		value = value / 2;
 	}
-	return (i - 1);
+	return (count);
 }
